60d0174184ae698be15e06df02530fecf8e7323e
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-06-27 15:50:54 +0200

    ADD: pivot tables and user_id FK

---
+++ database/migrations/2024_06_27_131503_create_restaurant_type_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurant_type', function (Blueprint $table) {
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('cascade');
+
+            $table->unsignedBigInteger('restaurant_id');
+            $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('cascade');
+
+            $table->primary(['type_id', 'restaurant_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurant_type');
+    }
+};
