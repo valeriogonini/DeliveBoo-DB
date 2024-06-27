3e9a4814f53bcf300ac66cbabb3484597c090ad9
Giulio998 <giulio_favia@hotmail.it>
2024-06-27 16:07:17 +0200

    Add: restaurants dishes relation

---
+++ database/migrations/2024_06_27_140334_add_foreign_restaurant_id_to_dishes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('dishes', function (Blueprint $table) {
+            $table->unsignedBigInteger('restaurant_id')->after('id');
+            $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('dishes', function (Blueprint $table) {
+            $table->dropForeign(['restaurant_id']);
+            $table->dropColumn('restaurant_id');
+        });
+    }
+};
