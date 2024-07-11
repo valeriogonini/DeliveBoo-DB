95b46dc306405986837b8ad46046851d5b4319c4
valeriogonini <valeriogonini@gmail.com>
2024-07-11 16:34:03 +0200

    modifica del titolo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>DeliveBoo Admin</title>
 
 
     <!-- Fonts -->
